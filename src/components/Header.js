f2a55d7a07dd554c139a9ad8ec2f1c9849ee998e
Salko <64722632+sarajlija@users.noreply.github.com>
2023-04-30 14:34:54 +0200

    up

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,33 +8,31 @@ import data from "../data.json"
 
 function Header({ windowWith }) {
   const [valueHeader, setValueHeader] = useState(0)
-  const [state, setState] = useState(false)
+
   const { name } = data[valueHeader]
   console.log(name)
 
   const handleClick = index => {
     setValueHeader(index)
   }
-  const showPlanet = () => {
-    setState(true)
-  }
 
   return (
-    <Navbar bg="" expand="sm">
+    <Navbar collapseOnSelect bg="" expand="sm">
       <Container className="align-items-center d-flex justify-content-md-center flex-sm-column flex-md-row justify-content-sm-between" bsPrefix="container__header">
         <div className="wrapper">
           <Navbar.Brand href="#" className="text-light">
             The planets
           </Navbar.Brand>
-
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         </div>
-        <Navbar.Collapse id="responsive-navbar-nav" className={`${console.log(state)}`}>
+
+        <Navbar.Collapse id="">
+          {" "}
           <Nav className="ms-auto my-2 my-lg-0">
             {data.map((item, index) => (
               <LinkContainer key={item.name} to={`/${item.name !== "Mercury" ? item.name.toLowerCase() : ""}`} onClick={() => handleClick(index)}>
                 <Nav.Item>
-                  <Nav.Link onClick={() => showPlanet()} href={`/${item.name !== "Mercury" ? item.name.toLowerCase() : ""}`} bsPrefix={`nav-link__${item.name.toLowerCase()}`} className={` ${index === valueHeader && "active"}`}>
+                  <Nav.Link href={`/${item.name !== "Mercury" ? item.name.toLowerCase() : ""}`} bsPrefix={`nav-link__${item.name.toLowerCase()}`} className={` ${index === valueHeader && "active"}`}>
                     {item.name}
                   </Nav.Link>
                 </Nav.Item>
